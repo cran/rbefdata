bcee92fb3f3953b6d75f31c40f4601c317ed7f64
Claas-Thido Pfaff <claas-thido.pfaff@idiv-biodiversity.de>
2013-04-17 00:00:00 +0000

    version 0.3.1

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad = function(lib, pkg) {
   .bef.env$.bef.opts = list(
-    url = "http://befdatadevelepment.biow.uni-leipzig.de",
+    url = "http://befdataproduction.biow.uni-leipzig.de",
     tematres_url = "http://befdataproduction.biow.uni-leipzig.de:7070/vocab/index.php",
     tematres_service_url = "http://befdataproduction.biow.uni-leipzig.de:7070/vocab/services.php",
     download_dir = "downloads"
